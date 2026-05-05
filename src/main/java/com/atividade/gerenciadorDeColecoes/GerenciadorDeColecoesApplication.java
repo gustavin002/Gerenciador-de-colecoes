4d0bc85c009d23e104da0763a94804888e2c3646
gustavin002 <gustavomaia957@gmail.com>
2026-05-05 14:32:20 -0300

    iniciando o projeto

---
+++ src/main/java/com/atividade/gerenciadorDeColecoes/GerenciadorDeColecoesApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.gerenciadorDeColecoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeColecoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeColecoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividade/gerenciadorDeColecoes/GerenciadorDeColecoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.gerenciadorDeColecoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeColecoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
